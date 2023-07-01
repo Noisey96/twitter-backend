a734234001a1c6a02d0d8f126c032be2aa49d498
Noisey96 <jlfreeman96@gmail.com>
2023-06-30 20:15:32 -0400

    Remove old console logging code

--- src/services/databaseService.ts
+++ src/services/databaseService.ts
@@ -1,17 +1,9 @@
 import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
-import { DefaultLogger, LogWriter } from 'drizzle-orm';
 
 import * as schema from '../../src/db/schema';
 
-class ConsoleLogWriter implements LogWriter {
-	write(message: string): void {
-		console.log(message);
-	}
-}
-
 export function connectToDatabase(databaseUrl: string) {
 	const queryClient = new Pool({ connectionString: databaseUrl });
-	const logger = new DefaultLogger({ writer: new ConsoleLogWriter() });
-	return drizzle(queryClient, { schema, logger });
+	return drizzle(queryClient, { schema });
 }
